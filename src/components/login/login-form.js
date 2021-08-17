4abdcbe802bb7326b1d3c8f95cb4c43aab9e9401
Trisha <trisha@reggora.com>
2021-08-16 20:11:04 -0400

    started the handleSubmit function

--- src/components/login/login-form.js
+++ src/components/login/login-form.js
@@ -6,8 +6,29 @@ export const LoginForm = () => {
     const [password, setPassword] = useState("");
 
     const handleSubmit = e => {
+        console.log('handleSubmit')
+        e.preventDefault(e);
+        setUsername(username);
         // you'd want to fetch via `${API_BASE_URL}/auth/signin` and return the auth token, which will then get set on localStorage of browser
-        return {}
+
+        const headers = {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        }
+
+        return fetch(`${API_BASE_URL}/auth/signin`, {
+            method: 'POST',
+            headers,
+            body: JSON.stringify({
+                username,
+                password
+            })
+        })
+        .then(res => {
+            const r = res.json()
+            console.log('res:', r)
+            return r
+        })
     }
 
     return(
@@ -24,7 +45,7 @@ export const LoginForm = () => {
                 placeholder="enter username"
                 type="text"
                 name="username"
-                pattern="[A-Za-z0-9_]{1,15}"
+                // pattern="[A-Za-z0-9_]{1,15}"
                 title="Username should only contain letters, numbers and underscores; no more than 15 characters e.g. trisha_123"
                 id="login-username"
                 required
@@ -36,7 +57,7 @@ export const LoginForm = () => {
                 placeholder="enter password"
                 type="password"
                 name="password"
-                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$"
+                // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$"
                 title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
                 required
                 id="login-password"
